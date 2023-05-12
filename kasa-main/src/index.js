43c53326ac6bcbdb07f55e83aa5ce0963477df42
kelly Simon <simon.kelly0820@gmail.com>
2023-05-12 12:43:18 +0200

    Router + pages application

--- kasa-main/src/index.js
+++ kasa-main/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import "./styles/index.css";
-import Home from './pages/Home.jsx';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//On importe toutes nos pages
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Card from './pages/Card.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+        <Route path="/about" element={<About />} errorElement={<ErrorPage />} />
+        <Route path="/logement" element={<Card />} errorElement={<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
