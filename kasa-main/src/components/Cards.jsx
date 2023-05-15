29655f4ad36620e9474333d3f223bca09b690912
kelly Simon <simon.kelly0820@gmail.com>
2023-05-15 17:59:15 +0200

    Création page accueil - code style sass

--- kasa-main/src/components/Cards.jsx
+++ kasa-main/src/components/Cards.jsx
@@ -5,7 +5,6 @@ function Cards({ image, title }) {
     return (
         <div className="cards">
             <img src={image} alt="" className="cards__img" />
-            <div className="cards__cover"></div>
             <h2 className="cards__title">{title}</h2>
         </div>
     )
